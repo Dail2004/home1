ff3c09722e45424657f3e0ede7360135e306af8b
Dail2004 <plusulta20@gmail.com>
2021-06-19 18:05:42 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String homeWork;
+        final int NUM = -515;
+        String word = "время";
+        homeWork = NUM + word;
+        System.out.println(homeWork);
+        if (NUM > 0) {
+            System.out.println("Вы сохранили" + " " + "положительное число");
+            boolean b = NUM < 0;
+            System.out.println("Вы сохранили" + " " +
+                    "отрецательное число");
+        } else {
+            System.out.println("Вы сохранили нуль");
+        }
+
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.print("привет " + name);
+            }
+        }
+
